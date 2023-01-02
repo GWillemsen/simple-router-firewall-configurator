c5d4884261d9feef70941fbd7c5ec409fc821ec1
GWillemsen <33036522+GWillemsen@users.noreply.github.com>
2023-01-02 21:23:36 +0100

    Fix clients not having internet access and port forward being broken

--- update_names.sh
+++ update_names.sh
@@ -62,17 +62,33 @@ generate_iptable_rules() {
     for PORT in "${PORTS[@]}"
     do
         # Allow incoming connections on the port
-        $IPTABLES -A INPUT -i ${PUB_IF} -p tcp --dport $PORT -m state --state NEW,ESTABLISHED -j ACCEPT
-        # Allow responses on the port
-        $IPTABLES -A OUTPUT -o ${PUB_IF} -p tcp --dport $PORT -m state --state ESTABLISHED -j ACCEPT
-        
-        $IPTABLES -A FORWARD -p tcp --dport $PORT -d $ADDRESS -j ACCEPT
-        # If only from external then add -i ${PUB_IF}, if allow access for both public and private through router then just leave it out.
-        $IPTABLES -t nat -A PREROUTING -p tcp --dport $PORT -j DNAT --to $ADDRESS:$PORT
-        # Hairpin NAT
-        $IPTABLES -t nat -A POSTROUTING -s $LOCAL_IP -o ${PRI_IF} -p tcp --dport $PORT -j MASQUERADE
+        # $IPTABLES -A INPUT -i ${PUB_IF} -p tcp --dport $PORT -m state --state NEW,ESTABLISHED -j ACCEPT
+        $IPTABLES -A INPUT -p tcp --dport $PORT -j ACCEPT
 
-        #TODO Hashlimit?
+        # Allow responses on the port. for the public network it needs to be a established connection.
+        # Locally also new connection requests are allowed (for when someone uses the internal IP instead of
+        # public)
+        $IPTABLES -A OUTPUT -o ${PUB_IF} -p tcp --dport $PORT -m state --state ESTABLISHED -j ACCEPT
+        $IPTABLES -A OUTPUT -o ${PRI_IF} -p tcp --dport $PORT -m state --state NEW,ESTABLISHED -j ACCEPT
+        
+        # Forward the packets internally when the input has accepted them
+        # This applies both for packets from the outside as internal.
+        $IPTABLES -A FORWARD -p tcp --dport $PORT -d $ADDRESS -j ACCEPT
+
+        # Reroute packates from the outside world to the respective ip & port if they are going to that port on routers' port
+        $IPTABLES -t nat -A PREROUTING -i ${PUB_IF} -p tcp --dport $PORT -j DNAT --to-destination $ADDRESS:$PORT
+        $IPTABLES -t nat -A PREROUTING -i ${PUB_IF} -p udp --dport $PORT -j DNAT --to-destination $ADDRESS:$PORT
+        
+        # Hairpin NAT part 1.
+        # If the destination is ourselfs but using the public IP then do an immediate destination rewrite.
+        $IPTABLES -t nat -A PREROUTING -i ${PRI_IF} -d ${PUBLIC_IP} -p tcp --dport $PORT -j DNAT --to-destination $ADDRESS:$PORT
+        $IPTABLES -t nat -A PREROUTING -i ${PUB_IF} -d ${PUBLIC_IP} -p udp --dport $PORT -j DNAT --to-destination $ADDRESS:$PORT
+        
+        # Hairpin NAT part 2.
+        # When the packet has been rerouted to correct local IP then if the source IP was also a local IP then masquarade the 
+        # source IP otherwise the original sender gets confused because the src ip would be different than the one send.
+        $IPTABLES -t nat -A POSTROUTING -s $LOCAL_IP -o ${PRI_IF} -p tcp --dport $PORT -j MASQUERADE
+        $IPTABLES -t nat -A POSTROUTING -s $LOCAL_IP -o ${PRI_IF} -p udp --dport $PORT -j MASQUERADE
     done
 }
 
@@ -86,8 +102,9 @@ set_default_iptables_rules() {
         $IPTABLES -A INPUT -i ${PUB_IF} -p tcp -m state --state ESTABLISHED,RELATED -j ACCEPT
         $IPTABLES -A INPUT -i ${PUB_IF} -p udp -m state --state ESTABLISHED,RELATED -j ACCEPT
     
-        # $IPTABLES -A INPUT -i ${PUB_IF} -j ACCEPT
-        # $IPTABLES -A OUTPUT -o ${PRI_IF} -j ACCEPT
+        # If the packet is from the local network allow anything. We assume that our local network is kind of safe
+        # and because we don't know yet if this is a packet that we have to route (NAT) to the outside or not.
+        $IPTABLES -A INPUT -i ${PRI_IF} -j ACCEPT
     }
     
     configure_icmp_ping_pong_request() {
@@ -97,6 +114,8 @@ set_default_iptables_rules() {
         $IPTABLES -A INPUT -i ${PUB_IF} -p icmp --icmp-type echo-reply -j ACCEPT
         
         # Allow everyone on the local interface to ping us, unlike on the WAN interface.
+        # We kinda trust our local network and while not strictly needed it does help with 
+        # debugging issues quite a lot.
         $IPTABLES -A OUTPUT -o ${PRI_IF} -p icmp -j ACCEPT
         $IPTABLES -A INPUT -i ${PRI_IF} -p icmp -j ACCEPT
     }
@@ -126,7 +145,12 @@ set_default_iptables_rules() {
     }
 
     configure_nat() {
-        $IPTABLES -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+        # If the packet is from the local network and the destination is the outside world then
+        # masquerade the IP (ie do NAT).
+        $IPTABLES -t nat -A POSTROUTING -s ${LOCAL_IP} -o eth0 -j MASQUERADE
+
+        # Internally forward packets from the local network that (might) need to 
+        # be routed to the outside world.
         $IPTABLES -A FORWARD -i ${PRI_IF} -j ACCEPT
 
         # Because the default policy is drop we need to explicity accept 
@@ -139,7 +163,6 @@ set_default_iptables_rules() {
     configure_dhcp_protocol
     configure_dns_protocol
     configure_nat
-
 
     $IPTABLES -P INPUT DROP
     $IPTABLES -P OUTPUT DROP
