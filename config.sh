cd257c9f7f350369f88b516257709d96b01c8299
GWillemsen <33036522+GWillemsen@users.noreply.github.com>
2023-01-02 21:59:02 +0100

    Consistent name + full UDP support

--- config.sh
+++ config.sh
@@ -3,8 +3,10 @@ export HOSTS_FILE=./hosts
 export DHCP_HOSTS_FILE=./hosts.dhcp
 export DOMAIN=internal
 export DEBUG=0
-export LOCAL_IP="192.168.1.0/24"
+export LOCAL_IP="192.168.1.1"
+export LOCAL_NETWORK="192.168.1.0/24"
 export PUBLIC_IP="192.168.68.240"
+export PUBLIC_NETWORK="192.168.68.0/24"
 export PUB_IF=eth0
 export PRI_IF=eth1
 
--- update_names.sh
+++ update_names.sh
@@ -62,100 +62,104 @@ generate_iptable_rules() {
     for PORT in "${PORTS[@]}"
     do
         # Allow incoming connections on the port
-        # $IPTABLES -A INPUT -i ${PUB_IF} -p tcp --dport $PORT -m state --state NEW,ESTABLISHED -j ACCEPT
+        # $IPTABLES -A INPUT -i $PUB_IF -p tcp --dport $PORT -m state --state NEW,ESTABLISHED -j ACCEPT
         $IPTABLES -A INPUT -p tcp --dport $PORT -j ACCEPT
+        $IPTABLES -A INPUT -p udp --dport $PORT -j ACCEPT
 
         # Allow responses on the port. for the public network it needs to be a established connection.
         # Locally also new connection requests are allowed (for when someone uses the internal IP instead of
         # public)
-        $IPTABLES -A OUTPUT -o ${PUB_IF} -p tcp --dport $PORT -m state --state ESTABLISHED -j ACCEPT
-        $IPTABLES -A OUTPUT -o ${PRI_IF} -p tcp --dport $PORT -m state --state NEW,ESTABLISHED -j ACCEPT
+        $IPTABLES -A OUTPUT -o $PUB_IF -p tcp --dport $PORT -m state --state ESTABLISHED -j ACCEPT
+        $IPTABLES -A OUTPUT -o $PRI_IF -p tcp --dport $PORT -m state --state NEW,ESTABLISHED -j ACCEPT
+        $IPTABLES -A OUTPUT -o $PUB_IF -p udp --dport $PORT -m state --state ESTABLISHED -j ACCEPT
+        $IPTABLES -A OUTPUT -o $PRI_IF -p udp --dport $PORT -m state --state NEW,ESTABLISHED -j ACCEPT
         
         # Forward the packets internally when the input has accepted them
         # This applies both for packets from the outside as internal.
         $IPTABLES -A FORWARD -p tcp --dport $PORT -d $ADDRESS -j ACCEPT
+        $IPTABLES -A FORWARD -p udp --dport $PORT -d $ADDRESS -j ACCEPT
 
         # Reroute packates from the outside world to the respective ip & port if they are going to that port on routers' port
-        $IPTABLES -t nat -A PREROUTING -i ${PUB_IF} -p tcp --dport $PORT -j DNAT --to-destination $ADDRESS:$PORT
-        $IPTABLES -t nat -A PREROUTING -i ${PUB_IF} -p udp --dport $PORT -j DNAT --to-destination $ADDRESS:$PORT
+        $IPTABLES -t nat -A PREROUTING -i $PUB_IF -p tcp --dport $PORT -j DNAT --to-destination $ADDRESS:$PORT
+        $IPTABLES -t nat -A PREROUTING -i $PUB_IF -p udp --dport $PORT -j DNAT --to-destination $ADDRESS:$PORT
         
         # Hairpin NAT part 1.
         # If the destination is ourselfs but using the public IP then do an immediate destination rewrite.
-        $IPTABLES -t nat -A PREROUTING -i ${PRI_IF} -d ${PUBLIC_IP} -p tcp --dport $PORT -j DNAT --to-destination $ADDRESS:$PORT
-        $IPTABLES -t nat -A PREROUTING -i ${PUB_IF} -d ${PUBLIC_IP} -p udp --dport $PORT -j DNAT --to-destination $ADDRESS:$PORT
+        $IPTABLES -t nat -A PREROUTING -i $PRI_IF -p tcp -d $PUBLIC_IP --dport $PORT -j DNAT --to-destination $ADDRESS:$PORT
+        $IPTABLES -t nat -A PREROUTING -i $PRI_IF -p udp -d $PUBLIC_IP --dport $PORT -j DNAT --to-destination $ADDRESS:$PORT
         
         # Hairpin NAT part 2.
         # When the packet has been rerouted to correct local IP then if the source IP was also a local IP then masquarade the 
         # source IP otherwise the original sender gets confused because the src ip would be different than the one send.
-        $IPTABLES -t nat -A POSTROUTING -s $LOCAL_IP -o ${PRI_IF} -p tcp --dport $PORT -j MASQUERADE
-        $IPTABLES -t nat -A POSTROUTING -s $LOCAL_IP -o ${PRI_IF} -p udp --dport $PORT -j MASQUERADE
+        $IPTABLES -t nat -A POSTROUTING -s $LOCAL_NETWORK -o $PRI_IF -p tcp --dport $PORT -j MASQUERADE
+        $IPTABLES -t nat -A POSTROUTING -s $LOCAL_NETWORK -o $PRI_IF -p udp --dport $PORT -j MASQUERADE
     done
 }
 
 set_default_iptables_rules() {
     configure_self_instatiated_connections() {
         # Allow new outgoing connections
-        $IPTABLES -A OUTPUT -o ${PUB_IF} -p tcp -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
-        $IPTABLES -A OUTPUT -o ${PUB_IF} -p udp -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+        $IPTABLES -A OUTPUT -o $PUB_IF -p tcp -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+        $IPTABLES -A OUTPUT -o $PUB_IF -p udp -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
 
         # Allow incoming connections that we initiated.
-        $IPTABLES -A INPUT -i ${PUB_IF} -p tcp -m state --state ESTABLISHED,RELATED -j ACCEPT
-        $IPTABLES -A INPUT -i ${PUB_IF} -p udp -m state --state ESTABLISHED,RELATED -j ACCEPT
+        $IPTABLES -A INPUT -i $PUB_IF -p tcp -m state --state ESTABLISHED,RELATED -j ACCEPT
+        $IPTABLES -A INPUT -i $PUB_IF -p udp -m state --state ESTABLISHED,RELATED -j ACCEPT
     
         # If the packet is from the local network allow anything. We assume that our local network is kind of safe
         # and because we don't know yet if this is a packet that we have to route (NAT) to the outside or not.
-        $IPTABLES -A INPUT -i ${PRI_IF} -j ACCEPT
+        $IPTABLES -A INPUT -i $PRI_IF -j ACCEPT
     }
     
     configure_icmp_ping_pong_request() {
         # Allow outgoing echo requests and incoming echo replies.
         # This prevents others from pinging us.
-        $IPTABLES -A OUTPUT -o ${PUB_IF} -p icmp --icmp-type echo-request -j ACCEPT
-        $IPTABLES -A INPUT -i ${PUB_IF} -p icmp --icmp-type echo-reply -j ACCEPT
+        $IPTABLES -A OUTPUT -o $PUB_IF -p icmp --icmp-type echo-request -j ACCEPT
+        $IPTABLES -A INPUT -i $PUB_IF -p icmp --icmp-type echo-reply -j ACCEPT
         
         # Allow everyone on the local interface to ping us, unlike on the WAN interface.
         # We kinda trust our local network and while not strictly needed it does help with 
         # debugging issues quite a lot.
-        $IPTABLES -A OUTPUT -o ${PRI_IF} -p icmp -j ACCEPT
-        $IPTABLES -A INPUT -i ${PRI_IF} -p icmp -j ACCEPT
+        $IPTABLES -A OUTPUT -o $PRI_IF -p icmp -j ACCEPT
+        $IPTABLES -A INPUT -i $PRI_IF -p icmp -j ACCEPT
     }
 
     configure_dhcp_protocol() {
         # Accept DHCP requests and allow send response on the pri_if
-        $IPTABLES -A INPUT -i ${PRI_IF} -p udp --dport 67 -j ACCEPT
-        $IPTABLES -A OUTPUT -o ${PRI_IF} -p udp --dport 68 -j ACCEPT
+        $IPTABLES -A INPUT -i $PRI_IF -p udp --dport 67 -j ACCEPT
+        $IPTABLES -A OUTPUT -o $PRI_IF -p udp --dport 68 -j ACCEPT
     }
 
     configure_dns_protocol() {
         # Accept DNS requests on priv_if
-        $IPTABLES -A INPUT -i ${PRI_IF} -p udp --dport 53 -j ACCEPT
-        $IPTABLES -A INPUT -i ${PRI_IF} -p tcp --dport 53 -j ACCEPT
+        $IPTABLES -A INPUT -i $PRI_IF -p udp --dport 53 -j ACCEPT
+        $IPTABLES -A INPUT -i $PRI_IF -p tcp --dport 53 -j ACCEPT
 
         # Allow DNS responses on priv_if
-        $IPTABLES -A OUTPUT -o ${PRI_IF} -p udp --sport 53 -j ACCEPT
-        $IPTABLES -A OUTPUT -o ${PRI_IF} -p tcp --sport 53 -j ACCEPT
+        $IPTABLES -A OUTPUT -o $PRI_IF -p udp --sport 53 -j ACCEPT
+        $IPTABLES -A OUTPUT -o $PRI_IF -p tcp --sport 53 -j ACCEPT
 
         # Accept DNS requests on public if
-        $IPTABLES -A INPUT -i ${PUB_IF} -p udp --dport 53 -j ACCEPT
-        $IPTABLES -A INPUT -i ${PUB_IF} -p tcp --dport 53 -j ACCEPT
+        $IPTABLES -A INPUT -i $PUB_IF -p udp --dport 53 -j ACCEPT
+        $IPTABLES -A INPUT -i $PUB_IF -p tcp --dport 53 -j ACCEPT
 
         # Allow DNS responses on public if
-        $IPTABLES -A OUTPUT -o ${PUB_IF} -p udp --sport 53 -j ACCEPT
-        $IPTABLES -A OUTPUT -o ${PUB_IF} -p tcp --sport 53 -j ACCEPT
+        $IPTABLES -A OUTPUT -o $PUB_IF -p udp --sport 53 -j ACCEPT
+        $IPTABLES -A OUTPUT -o $PUB_IF -p tcp --sport 53 -j ACCEPT
     }
 
     configure_nat() {
         # If the packet is from the local network and the destination is the outside world then
         # masquerade the IP (ie do NAT).
-        $IPTABLES -t nat -A POSTROUTING -s ${LOCAL_IP} -o eth0 -j MASQUERADE
+        $IPTABLES -t nat -A POSTROUTING -s $LOCAL_NETWORK -o eth0 -j MASQUERADE
 
         # Internally forward packets from the local network that (might) need to 
         # be routed to the outside world.
-        $IPTABLES -A FORWARD -i ${PRI_IF} -j ACCEPT
+        $IPTABLES -A FORWARD -i $PRI_IF -j ACCEPT
 
         # Because the default policy is drop we need to explicity accept 
         # traffic that was routed through NAT to the local network.
-        $IPTABLES -A FORWARD -o ${PRI_IF} -j ACCEPT
+        $IPTABLES -A FORWARD -o $PRI_IF -j ACCEPT
     }
 
     configure_self_instatiated_connections
@@ -184,10 +188,10 @@ for i in "${INI__ALL_SECTIONS[@]}"
 do
     get_node_config $i
     if [ $DEBUG == "1" ]; then
-        echo N: ${NAME}
-        echo A: ${ADDRESS}
-        echo M: ${MAC}
-        echo P: ${PORTS}
+        echo N: $NAME
+        echo A: $ADDRESS
+        echo M: $MAC
+        echo P: $PORTS
     fi
 
     add_to_hosts
